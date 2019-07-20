6cdc95fad787a840fc5e82269f2b4ba5a162be0f
Anthony Gitter <gitter@biostat.wisc.edu>
2019-07-20 06:11:30 -0500

    Add light logging

--- update-readme.sh
+++ update-readme.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 pattern=content/*
+base=README-BASE.md
 
-cp README-BASE.md README.md
+printf 'Initializing README.md from %s\n' $base
+cp $base README.md
 
 for file in $pattern
 do
+  printf 'Extracting references from %s\n' $file
   printf '## %s\n' $(basename $file) >> README.md
   manubot cite --render --format=markdown $(tr '\n' ' ' < $file) >> README.md
   printf '\n\n' >> README.md
